d6a33dabd1cdc1b95b46983ccdb6cd37ae1c251f
Nehara831 <112460919+Nehara831@users.noreply.github.com>
2023-12-21 21:42:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/navbar';
 
 import Footer from './components/footer/Footer';
 
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="App">
     <Footer/>
     </div>
+=======
+    <Navbar/>
+>>>>>>> b5c28d4 (navbar component)
   );
 }
 
