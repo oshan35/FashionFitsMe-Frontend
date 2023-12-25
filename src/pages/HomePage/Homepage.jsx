483faf91aa1425d41d1f3152b1e690b9a7ba3088
oshan35 <oshan.20191052@iit.ac.lk>
2023-12-25 23:16:21 +0530

    mobile responsive media css changes

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -25,44 +25,55 @@ const HomePage = () => {
             <Flex  className="nav-container">
 
                 <Navbar className="nav-item"/>
-                </Flex>
-                <Flex horizontal="horizontal" className="grid-item1">
-            <Flex  className="image1"> </Flex>
-            <Flex  className="image2"> </Flex>
-         
             </Flex>
-            <Flex horizontal="horizontal" className="grid-item">
-            <Flex  className="image3"> </Flex>
+            <Flex horizontal="horizontal" className="grid-item1">
+                <Flex  className="image1"> </Flex>
+                <Flex  className="image2"> </Flex>
             
             </Flex>
             <Flex horizontal="horizontal" className="grid-item">
-            <Flex  className="image4"> </Flex>
-            <Flex  className="image5"> </Flex>
+                <Flex  className="image3"> </Flex>
+            
+            </Flex>
+            <Flex horizontal="horizontal" className="grid-item">
+                <Flex  className="image4"> </Flex>
+                <Flex  className="image5"> </Flex>
             </Flex>
             <Flex vertical="vertical " className="grid-item-brands">
 
                 <Flex horizontal="horizontal" className="brands-top">
-                <Flex  className="brand10"> </Flex>
-                   
-                    <Flex  className="brand8"> </Flex>
-                    <Flex  className="brand7"> </Flex>
-                    <Flex  className="brand2"> </Flex>
-                    <Flex  className="brand3"> </Flex>
-                    
-            
+                    <Flex  className="brand-icon" id="brand10">
+                        <img src={image10} className="brand-image"/>
+                    </Flex>
+                    <Flex  className="brand-icon" id="brand8">
+                        <img src={image8} className="brand-image"/>
+                    </Flex>
+                    <Flex  className="brand-icon" id="brand7">
+                        <img src={image7} className="brand-image"/>
+                    </Flex>
+                    <Flex  className="brand-icon" id="brand2"> 
+                        <img src={image2} className="brand-image"/>
+                    </Flex>
+                    <Flex  className="brand-icon" id="brand3">
+                        <img src={image3} className="brand-image"/>
+                    </Flex>
                 </Flex>
                 <Flex horizontal="horizontal" className="brands-bottom">
-                    <Flex  className="brand4"> </Flex>
-                    <Flex  className="brand5"> </Flex>
-                    <Flex  className="brand9"> </Flex>
-                    <Flex  className="brand1"> </Flex>
-                    <Flex  className="brand6"> </Flex>
-                   
-                   
-                 
-                   
-            
-
+                    <Flex  className="brand-icon" id="brand4">
+                        <img src={image4} className="brand-image"/>
+                    </Flex>
+                    <Flex  className="brand-icon" id="brand5">
+                        <img src={image5} className="brand-image"/>
+                    </Flex>
+                    <Flex  className="brand-icon" id="brand9">
+                        <img src={image9} className="brand-image"/>
+                    </Flex>
+                    <Flex  className="brand-icon" id="brand1">
+                        <img src={image1} className="brand-image"/>
+                    </Flex>
+                    <Flex  className="brand-icon" id="brand6">
+                        <img src={image6} className="brand-image"/>
+                    </Flex>
                 </Flex>
             </Flex>
             <Flex className="grid-item"></Flex>
