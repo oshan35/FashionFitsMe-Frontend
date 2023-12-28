bf2e3d641e80c200fb4069b24d3ad993db4898a1
ashen829 <ashennethsara99@gmail.com>
2023-12-28 22:39:55 +0530

    signup page frontend

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,71 +1,103 @@
-import React, { useState } from 'react';
+import React from "react";
+import "./SignupPage.css";
+import { LockOutlined, UserOutlined, MobileOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
 import Navbar from '../../components/navbar/navbar';
-import Footer from '../../components/footer/Footer'; 
-import './SignupPage.css';
+import Footer from '../../components/footer/Footer';
 
-function SignupPage() {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+const App = () => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your signup logic here
-    console.log('Form submitted:', formData);
-    // Reset the form after submission
-    setFormData({ username: '', email: '', password: '' });
-  };
 
   return (
-    <div className="App">
-      <Navbar />
-      <h1>Signup Page</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
-        <button type="submit">Signup</button>
-      </form>
-      <Footer />
-    </div>
-  );
-}
+    <Form classname="loginform" >
 
-export default SignupPage;
+<Navbar/>
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+  <hi className="heading">CREATE ACCOUNT</hi>
+    
+      <Form.Item
+        name="Firstname"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input your First Name!",
+          },
+        ]}
+      >
+        <Input placeholder="First Name"/>
+      </Form.Item>
+
+
+      <Form.Item
+        name="Lastname"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Last Name!",
+          },
+        ]}
+      >
+        <Input  placeholder="Last Name" />
+      </Form.Item>
+
+
+      <Form.Item
+        name="username"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Username!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Userame"
+        />
+      </Form.Item>
+
+
+      <Form.Item
+        name="password"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Password!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Sign in
+        </Button>
+      </Form.Item>
+
+    </Form>
+    <Footer/>
+    </Form>
+  );
+};
+export default App;
