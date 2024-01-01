072c3b431c1cac8e87a7aae9e5e317a1a6eb7e03
Nehara831 <112460919+Nehara831@users.noreply.github.com>
2024-01-02 00:30:20 +0530

    clothcard added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/navbar';
-
+import PriceCard from './components/pricecard/pricecard';
 import Footer from './components/footer/Footer';
 import HomePage from './pages/HomePage/Homepage';
-
+import ClothCard from './components/clothCard';
+import testimg from './asserts/76J4405_IYI_FNT________________________________.jpeg'
 function App() {
+
+  const item = {
+    itemName: 'FAVORITE STRIPE T-SHIRT',
+    itemColour: 'Classic Pink Multi ',
+    itemSize: 'xxs',
+    inStock:'2'
+};
+
   return (
     <div className="App">
-    <HomePage/>
+    <ClothCard picture={testimg} itemDescription={item}/>
     </div>
   );
 }
---
+++ src/components/clothCard.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Flex, Button} from 'antd';
+import './clothCard.css'
+import { Select } from 'antd';
+const ClothCard = ({ picture, itemDescription }) => {
+
+    const { Option } = Select;
+    
+    return (
+        <>
+            <Flex horizontal='horizontal' className='main-container'>
+                <Flex className='picture' justify='center'>
+                    {picture && <img src={picture} alt="Price Card"/>}
+                </Flex>
+                <Flex vertical='vertical' justify='center' className='no-picture'>
+                    <Flex vertical='vertical' justify='center' className='top-part'>
+                        
+                            <Flex vertical='vertical' justify='center' className='container'>
+                                <Flex horizontal='horizontal' className='description'>
+                                    <p className='product-name'>
+                                            {itemDescription.itemName}
+                                    </p>
+                                    <Select
+                                        defaultValue="Qty"
+                                        className='select'
+                                        bordered
+                                        style={{ width: 80, borderRadius: 4 }}
+                                        dropdownStyle={{ borderRadius: 4 }}
+                                    >
+                                        <Option value="1">1</Option>
+                                        <Option value="2">2</Option>
+                                        <Option value="3">3</Option>
+                                        <Option value="4">4</Option>
+                                        <Option value="5">5</Option>
+                                        <Option value="6">6</Option>
+                                    </Select>
+                                </Flex>
+                                <Flex vertical='vertical' justify='center' className='container-vertical'>
+                                    <p className='additional-details'>
+                                        {itemDescription.itemColour} | {itemDescription.itemSize}
+                                    </p>
+                                    <p className='availability'>
+                                        {itemDescription.inStock > 0 ? `Only ${itemDescription.inStock} left in stock` : 'Out of stock'} | Ships in 1-2 business days
+                                    </p>
+                                    <p className='shipping-info'></p>
+                                </Flex>
+                               
+                                
+                            </Flex>
+                            
+                        
+                    </Flex>
+                        <Flex horizontal='horizontal' className='bottom-part'>
+                            <p className='underline-on-hover' >
+                                Edit
+                            </p>
+                            <p className='underline-on-hover' >
+                                Remove
+                            </p>
+                        </Flex>
+                </Flex>
+            </Flex>
+        </>
+    )
+}
+
+export default ClothCard
