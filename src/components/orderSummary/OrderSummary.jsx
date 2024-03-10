f02cb2594451371ffdf83df9863bf75d84b57793
ashen829 <ashennethsara99@gmail.com>
2024-03-11 00:49:31 +0530

    Product page

--- src/components/orderSummary/OrderSummary.jsx
+++ src/components/orderSummary/OrderSummary.jsx
@@ -1,12 +1,7 @@
 import {Flex} from 'antd';
 import React from 'react';
 import './OrderSummary.css';
-import { Card } from 'antd';
-import Navbar from '../../components/navbar/navbar';
-import Footer from '../../components/footer/Footer';
-import ClothCard from '../clothCard/ClothCard';
-import PriceCard from '../../components/pricecard/pricecard'
-import testimg from '../../asserts/76J4405_IYI_FNT________________________________.jpeg'
+
 
 const OrderSummary = ({orderInformation}) => {
     
@@ -19,21 +14,21 @@ const OrderSummary = ({orderInformation}) => {
             <Flex><h2>ORDER SUMMARY</h2></Flex>
             <Flex horizontal="horizontal" className="summary-item">
                 <p>Subtotal</p>
-                <p>{orderInformation.subTotal}</p>
+
             </Flex>
             <Flex horizontal="horizontal" className="summary-item">
                 <p>Discount</p>
-                <p>{orderInformation.discount}</p>
+
             </Flex>
             <Flex horizontal="horizontal" className="summary-item">
                 <p>Tax</p>
-                <p>Calculated in Checkout</p>
+
             </Flex>
             <Flex className="horizontal-line" > <hr /></Flex>
             
             <Flex horizontal="horizontal" className="summary-item">
                  <p>Estimated Total</p>
-                 <p>{orderInformation.estimatedTotal}</p>
+
             </Flex>
            
 
--- src/pages/AdminProduct/AdminProduct.jsx
+++ src/pages/AdminProduct/AdminProduct.jsx
@@ -1,18 +1,41 @@
 import React from "react";
-import { Flex, Button, Checkbox, Form, Input  } from 'antd';
+import { Flex, Form, Input, Upload,Select, Space, Col, Row , Button} from 'antd';
+import { PlusOutlined ,  SaveOutlined} from '@ant-design/icons';
 import "./AdminProduct.css";
 
-
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
 
 const App = () => {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
 
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+
+  const options = [];
+  for (let i = 10; i < 36; i++) {
+    options.push({
+      label: i.toString(36) + i,
+      value: i.toString(36) + i,
+    });
+  }
+  
+
 
   return (
     <>
-    <Form classname="loginform" >
+    <Form classname="loginform"
+    initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish} >
     <Flex horizontal="horizontal">
     <Flex vertical="vertical" >
     <Flex vertical="vertical" className="grid-cell19">
@@ -40,9 +63,9 @@ const App = () => {
       <div className="text3">Shop Profile</div>
       <div className="text4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
 
-
-      <Flex horizontal="horizontal"> 
-      <Form.Item    
+      <Row>
+        <Col span={12}>
+        <Form.Item    
         name="Shopname"
         className="textbox"
         rules={[
@@ -55,8 +78,9 @@ const App = () => {
         <div className="text5">Shop Name</div>
         <Input placeholder="Shop Name"/>
       </Form.Item>
-
-      <Form.Item    
+        </Col>
+        <Col span={12}>
+        <Form.Item    
         name="Phone"
         className="textbox"
         rules={[
@@ -69,10 +93,12 @@ const App = () => {
         <div className="text5">Phone</div>
         <Input placeholder="Mobile Number"/>
       </Form.Item>
-      </Flex>  
+        </Col>
+      </Row>
 
-      <Flex horizontal="horizontal"> 
-      <Form.Item    
+      <Row>
+        <Col span={12}>
+        <Form.Item    
         name="Owner"
         className="textbox"
         rules={[
@@ -85,8 +111,9 @@ const App = () => {
         <div className="text5">Owner</div>
         <Input placeholder="Owner's Name"/>
       </Form.Item>
-
-      <Form.Item    
+        </Col>
+        <Col span={12}>
+        <Form.Item    
         name="ShopId"
         className="textbox"
         rules={[
@@ -98,10 +125,363 @@ const App = () => {
       >
         <div className="text5">Shop ID</div>
         <Input placeholder="Shop ID"/>
-      </Form.Item>      
-    </Flex> 
+      </Form.Item>   
+        </Col>
+      </Row>
 
-    
+      <Row>
+        <Col span={12}>
+          <Form.Item
+      className="upload-logo"
+      name="logo"
+      valuePropName="fileList"
+      getValueFromEvent={normFile}
+      rules={[
+        {
+          required: true,
+          message: "Please upload a logo!",
+        },
+      ]}
+    >
+      <div className="text6">Shop Logo</div>
+      <Upload
+        action="/upload.do"
+        listType="picture-card"
+        maxCount={1} // Allow only one file to be uploaded
+      >
+        <button
+          style={{
+            border: 0,
+            background: "none",
+          }}
+          type="button"
+        >
+          <PlusOutlined />
+          <div
+            style={{
+              marginTop: 8,
+            }}
+          >
+            Upload
+          </div>
+        </button>
+      </Upload>
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item
+      className="brand-names"
+      rules={[
+        {
+          required: true,
+          message: "Please input brand names!",
+        },
+      ]}
+    >
+      <div className="text5">Brands</div>
+      <Space className="brands-textbox"
+      style={{
+        width: '100%',
+      }}
+      direction="vertical"
+    >
+      <Select
+        mode="multiple"
+        allowClear
+        style={{
+          width: '100%',
+        }}
+        placeholder="Please select"
+        defaultValue={['a10', 'c12']}
+        onChange={handleChange}
+        options={options}
+      />
+    </Space>
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col span={12}>
+        <Form.Item    
+        name="Location"
+        className="location-textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input your location!",
+          },
+        ]}
+      >
+        <div className="text5">Location</div>
+        <Input className="location-input" placeholder="Select your location"/>
+      </Form.Item>
+        </Col>
+        <Col span={12}>
+        <Form.Item    
+        name="Address"
+        className="address-textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Address!",
+          },
+        ]}
+      >
+        <div className="text5">Address</div>
+        <Input className="address-input" placeholder="Enter your address"/>
+      </Form.Item>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col span={12}>
+        <Form.Item    
+        name="Email"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input your email!",
+          },
+        ]}
+      >
+        <div className="text5">Email</div>
+        <Input placeholder="Email Address"/>
+      </Form.Item>
+        </Col>
+        <Col span={12}>
+        <Form.Item    
+        name="Facebook"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Facebook URL!",
+          },
+        ]}
+      >
+        <div className="text5">Facebook URL</div>
+        <Input  placeholder="Enter your Facebook URL"/>
+      </Form.Item>
+        </Col>
+      </Row>
+      
+      <Button className="save-button" type="primary" icon={<SaveOutlined />}>
+        Save
+      </Button>
+      <hr className="line" />  
+
+      <Flex vertical="vertical" >
+      <div className="text3">Add Products</div>
+      <div className="text4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+
+      <Row>
+        <Col span={12}>
+        <Form.Item    
+        name="Itemname"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input Item name",
+          },
+        ]}
+      >
+        <div className="text5">Item Name</div>
+        <Input placeholder="Eg: Premium Linen Short Sleeve Shirt"/>
+      </Form.Item>
+        </Col>
+        <Col span={12}>
+        <Form.Item    
+        name="Item Category"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input item category!",
+          },
+        ]}
+      >
+        <div className="text5">Item Category</div>
+        <Input placeholder="Eg: Men’s Wear"/>
+      </Form.Item>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col span={12}>
+        <Form.Item    
+        name="Itemtype"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input item type!",
+          },
+        ]}
+      >
+        <div className="text5">Item Type</div>
+        <Input placeholder="Eg: T-Shirt"/>
+      </Form.Item>
+        </Col>
+        <Col span={12}>
+        <Form.Item    
+        name="Brand"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please Select Brand!",
+          },
+        ]}
+      >
+        <div className="text5">Item Brand</div>
+        <Input placeholder="Eg: Odel"/>
+      </Form.Item>   
+        </Col>
+      </Row>
+
+      <Row>
+        <Col span={12}>
+        <Form.Item    
+        name="Color"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please input color of the item!",
+          },
+        ]}
+      >
+        <div className="text5">Color</div>
+        <Input placeholder="Eg: Light Green"/>
+      </Form.Item>
+        </Col>
+        <Col >
+        <Form.Item    
+        name="Material"
+        className="textbox"
+        rules={[
+          {
+            required: true,
+            message: "Please enter the material!",
+          },
+        ]}
+      >
+        <div className="text5">Material</div>
+        <Input placeholder="Eg: Baby Crocadile"/>
+      </Form.Item>   
+        </Col>
+      </Row>
+
+      <Row>
+        <Col >
+        <Form.Item    
+        name="No. of Items"
+        className="textbox1"
+        rules={[
+          {
+            required: true,
+            message: "Please input number of items!",
+          },
+        ]}
+      >
+        <div className="text5">No. of Items</div>
+        <Input placeholder="Eg: 20"/>
+      </Form.Item>
+        </Col>
+        <Col>
+        <Form.Item    
+        name="Size"
+        className="textbox1"
+        rules={[
+          {
+            required: true,
+            message: "Please select the Size!",
+          },
+        ]}
+      >
+        <div className="text5">Size</div>
+        <Input placeholder="Eg: Extra Small"/>
+      </Form.Item>   
+        </Col>
+
+        <Col>
+        <Form.Item    
+        name="Price"
+        className="textbox1"
+        rules={[
+          {
+            required: true,
+            message: "Please enter the price!",
+          },
+        ]}
+      >
+        <div className="text5">Unit Price</div>
+        <Input placeholder="Eg: Rs.7,990.00"/>
+      </Form.Item>   
+        </Col>
+      </Row>
+
+      <Row>
+        <Form.Item
+        className="upload-images"
+        name="logo"
+        valuePropName="fileList"
+        getValueFromEvent={normFile}
+        rules={[
+        {
+          required: true,
+          message: "Please upload a logo!",
+        },
+      ]}
+    >
+      <div className="text6">Images</div>
+      <Upload
+        action="/upload.do"
+        listType="picture-card"
+        maxCount={5}
+      >
+        <button
+          style={{
+            border: 0,
+            background: "none",
+          }}
+          type="button"
+        >
+          <PlusOutlined />
+          <div
+            style={{
+              marginTop: 8,
+            }}
+          >
+            Upload
+          </div>
+        </button>
+      </Upload>
+          </Form.Item>
+      </Row>
+      
+      <Button className="save-button" type="primary" icon={<SaveOutlined />}>
+        Save
+      </Button>
+      <hr className="line" />  
+    </Flex>
+    <Flex vertical="vertical">
+      <div className="text3">Payment Details</div>
+      <div className="dummy-text1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+
+      <hr className="line" />  
+    </Flex>
+
+    <Flex vertical="vertical" >
+      <div className="text3">Security</div>
+      <div className="dummy-text1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+
+      <hr className="line" />  
+    </Flex>
+
     </Flex>
     </Flex>
     </Form>
